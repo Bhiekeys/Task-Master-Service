886384ea2a876f874497bb0029d0cb4a9184b260
Dannyfsp <femoscopilala@gmail.com>
2024-06-16 20:13:27 +0100

    feat: data models

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,6 +2,8 @@ import mongoose, { Schema, Document } from "mongoose";
 
 export interface IUser extends Document {
     _id: string
+    firstName: string
+    lastName: string
     email: string
     password: string
     role: string
